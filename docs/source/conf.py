dc9565fe4b25729348332a112e61800e9cc07eab
bercowskya <bercowskya@gmail.com>
2022-01-26 16:39:23 +0100

    Added paleontologist logo
    Also changed the confy.py so that the logo can be displayed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,3 +52,10 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Logo
+html_logo = 'paleo_logo.png'
+html_theme_options = {
+	'logo_only' : True,
+	'display_version' : False
+}
